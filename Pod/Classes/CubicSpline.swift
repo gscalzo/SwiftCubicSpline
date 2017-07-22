427f7b718e58c999eb12d242e76037eec3adb4c2
Dan <postster@gmail.com>
2017-07-22 16:40:10 -0700

    Swift 3 compatibility.

--- Pod/Classes/CubicSpline.swift
+++ Pod/Classes/CubicSpline.swift
@@ -11,7 +11,7 @@ import SAMCubicSpline
 public class CubicSpline{
     private let samCubicSpline: SAMCubicSpline
     public init(points: [CGPoint]){
-        samCubicSpline = SAMCubicSpline(points: points.map{ p in NSValue(CGPoint:p)})
+        samCubicSpline = SAMCubicSpline(points: points.map{ p in NSValue(cgPoint:p)})
     }
     
     public func interpolate(x: CGFloat) -> CGFloat {
